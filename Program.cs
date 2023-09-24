7a590ed50abbda896e4898e12472256758881f9e
bin <bin@DESKTOP-10F3EO4>
2023-09-24 11:21:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// This program is going to generate winning lottery numbers
+// For five numbers, choose 1-70 without any repeating values
+//For one number (the powerball number), choose 1-25, which can repeat from the aforementioned numbers 
+Console.WriteLine("Hello, World!");
